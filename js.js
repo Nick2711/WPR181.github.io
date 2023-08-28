c639f99ab00299a59b67fdcdb801c504f82033fc
Nick2711 <Nicholas2711kelly@gmail.com>
2023-08-28 14:06:53 +0200

    Addeed files

---
+++ js.js
@@ -0,0 +1,53 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+  
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+  }
+
+ 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  
+ 
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+ 
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+  }
+  
+ 
+  function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+  }
